040d56aee9375d972cb3b1d3284e44e01ec826e8
Juan Manuel Malaver <juanmalaver1511@gmail.com>
2025-11-21 20:59:32 -0500

    feat: Enhance login functionality and add dashboard component

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
-  templateUrl: './login.html',
-  styleUrl: './login.css',
+  imports: [CommonModule],
+  templateUrl: './login.html'
 })
 export class Login {
 
+  errorMessage: string = '';
+
+  login(usuario: string, password: string) {
+    const userTest = 'juanma';
+    const passTest = '1234';
+
+    if (usuario === userTest && password === passTest) {
+      this.errorMessage = '';
+      console.log('Inicio de sesión correcto');
+      // aquí puedes navegar a otra ruta si quieres
+      // this.router.navigate(['/dashboard']);
+    } else {
+      this.errorMessage = 'Usuario o contraseña incorrectos';
+    }
+  }
 }
